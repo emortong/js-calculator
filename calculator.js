c3796cc5882be0e0d9ef15e2ba5f02f3f4d65c7c
Estefania Morton <estefania.morton@gmail.com>
2016-12-06 16:44:42 -1000

    finished for real

--- calculator.js
+++ calculator.js
@@ -20,22 +20,23 @@
    * @return { Number }
    */
  function calculatorModule(x){
+    //private
     var memory = 0;
     var total = 0;
 
     var isValid = function(x) {
       if(typeof x === "number") {
         return true;
-        } else {
-          throw new Error("not a number");
-        }
+      } else {
+        throw new Error("not a number");
+      }
     }
-
+    //public
     return {
       load: function(x) {
         if(isValid(x)){
-        total = x;
-        return total;
+          total = x;
+          return total;
         }
       },
       getTotal: function() {
@@ -43,22 +44,22 @@
       },
       add: function(x) {
         if(isValid(x)){
-        return total += x;
+          return total += x;
         }
       },
       subtract: function(x) {
         if(isValid(x)){
-        return total -= x;
+          return total -= x;
         }
       },
       multiply: function(x) {
         if(isValid(x)){
-        return total *= x;
+          return total *= x;
         }
       },
       divide: function(x) {
         if(isValid(x)){
-        return total /= x;
+          return total /= x;
         }
       },
       recallMemory: function() {
@@ -70,9 +71,6 @@
       clearMemory: function() {
         memory = 0;
       }
-
-
-
     };
  }
 
